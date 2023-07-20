473165a72ecc8505a110844ab11f3c76b66fe76b
Arthur-Candeia <arthurheher@hotmail.com>
2023-07-20 14:27:06 -0300

    estrutura de requisicoes estruturada

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,58 @@
-import { useState } from "react"
-import fetchAPI from "./api/fetch"
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from "react"
+import fetchAPI, {fetchSwitchVersion} from "./api/fetch"
 
 function App() {
-  
-  const [version, setVersion] = useState('acf')
 
-  fetchAPI(version)
+  const [version, setVersion] = useState('acf')
+  const [name, setName] = useState('')
+  const [chapter, setChapter] = useState(0)
+  const [number, setNumber] = useState(0)
+  const [text, setText] = useState('')
+  const [abbrev, setAbbrev] = useState('')
+  const [requests, setRequests] = useState(0)
+
+  useEffect(() => {
+    fetchAPI(version).then((result) => {
+        const {name, chapter, number, text, abbrev} = result
+        setName(name)
+        setChapter(chapter)
+        setNumber(number)
+        setText(text)
+        setAbbrev(abbrev)
+    })
+  }, [requests])
+
+  function handleSubmit(ev: React.FormEvent) {
+    ev.preventDefault()
+    setRequests((counter) => counter+=1)
+    console.log(requests)
+  }
+
+  function switchVersion(newVersion: string) {
+    setVersion(newVersion)
+    fetchSwitchVersion(abbrev, chapter, number, newVersion).then((result) => setText(result)) 
+  }
 
   return (
     <>
       <h1>Gerador de Versículos </h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           <p>Versão:</p>
           <span>
-            <input type="radio" name="version" id="acf" onChange={(ev) => setVersion(ev.target.value)} value={'acf'}  defaultChecked/>
+            <input type="radio" name="version" id="acf" onChange={(ev) => switchVersion(ev.target.value)} value={'acf'}  defaultChecked/>
             <label htmlFor="acf">ACF</label>
           </span>
           <span>
-            <input type="radio" name="version" id="nvi" onChange={(ev) => setVersion(ev.target.value)}  value={'nvi'}/>
+            <input type="radio" name="version" id="nvi" onChange={(ev) => switchVersion(ev.target.value)}  value={'nvi'}/>
             <label htmlFor="nvi">NVI</label>
           </span>
         </div>
-        <textarea cols={80} rows={10} disabled></textarea>
+        <div id="show">
+          <p>{text}</p>
+          <span>{`${name} ${chapter}:${number}`}</span>
+        </div>
         <div id="buttons">
           <input type="submit" value="GERAR" />
           <button type="button">COPIAR</button>
--- src/api/fetch.ts
+++ src/api/fetch.ts
@@ -1,10 +1,28 @@
-export default async function fetchAPI(version: string) {
-  const result = await fetch(`https://www.abibliadigital.com.br/api/verses/${version}/random`)
+interface data {
+  name: string,
+  chapter: number,
+  number: number,
+  text: string,
+  abbrev: string
+}
+
+export default async function fetchAPI(version: string): Promise<data> {
+  const result = await fetch(`https://www.abibliadigital.com.br/api/verses/${version}/random`, {headers: header})
   const data = await result.json()
   console.log(data)
   const name = data.book.name
   const chapter = data.chapter
   const number = data.number
-  const content = data.content
-  return {name, chapter, number, content}
+  const text = data.text
+  const abbrev = data.book.abbrev.pt
+  const response: data = {name, chapter, number, text, abbrev}
+  return response
+}
+
+export async function fetchSwitchVersion(abbrev: string, chapter: number, number: number, version: string) {
+  const result = await fetch(`https://www.abibliadigital.com.br/api/verses/${version}/${abbrev}/${chapter}/${number}`, {headers: header})
+  const data = await result.json()
+  console.log(data)
+  const text: string = data.text
+  return text
 }
